26ffc0905994698b2d27f94c59309535be965e66
Brian Luft <brian@lincolnloop.com>
2020-02-28 11:33:34 -0800

    Configuration stuff, adding in netlify-cms

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,9 @@
-module.exports = function(eleventyConfig) {
-  // Output directory: _site
-
-  eleventyConfig.addPassthroughCopy("assets/css");
+module.exports = function (eleventyConfig) {
+    // Output directory: _site
+    eleventyConfig.addPassthroughCopy("admin/config.yml");
+    eleventyConfig.addPassthroughCopy("assets/css");
     eleventyConfig.addPassthroughCopy("assets/js");
-  eleventyConfig.addPassthroughCopy("assets/webfonts");
-
-  eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("assets/webfonts");
+    eleventyConfig.addPassthroughCopy("images");
 
 };
