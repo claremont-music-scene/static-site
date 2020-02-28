0a80e29c25dbe640cc393f111e23b9ab8a4168d8
Brian Luft <brian@lincolnloop.com>
2020-02-28 12:31:12 -0800

    Fixes problem with eleventy processing node_modules directory

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function (eleventyConfig) {
     // Output directory: _site
-    eleventyConfig.addPassthroughCopy("admin/config.yml");
-    eleventyConfig.addPassthroughCopy("assets/css");
+  eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("assets/css");
     eleventyConfig.addPassthroughCopy("assets/js");
     eleventyConfig.addPassthroughCopy("assets/webfonts");
     eleventyConfig.addPassthroughCopy("images");
